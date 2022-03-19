6269976ea16eacccde7a4bf7514919b2a9483701
StepanGulyaev <91908161+StepanGulyaev@users.noreply.github.com>
2022-03-19 11:00:46 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,16 +3,39 @@
 #include "Passenger.h"
 #include "Registration.h"
 #include "Queue.h"
+#include "Table.h"
+
+void get_info()
+    {
+    
+    }
+
+void algorithm()
+    {
+    Table* table = new_table();
+    create_table(table);
+    size_t time = 0;
+
+    #ifdef MAKE_LIST
+        List* buffer = new_queue();
+        create_queue(buffer);
+    #else   
+        Vector* buffer = new_queue();
+        create_queue(buffer);
+    #endif
+
+    while(1)
+        {
+        Passenger* pas = new_Passenger();
+        add_info(pas);
+
+        Registration* reg = new_registration();
+        add_passenger(reg,pas);
+
+        push(buffer,reg);
+        }
+    }
 
 int main()
     {
-    #ifdef MAKE_LIST
-        List* queue = new_queue();
-    #else
-        Vector* queue = new_queue();
-    #endif
-
-    fill_queue(queue);
-    print_queue(queue);
-    free_queue(queue);
     }
